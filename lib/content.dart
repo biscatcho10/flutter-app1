d9a8c1a6812f0264a19876b2ad4e404e968c4c36
thecluders <trainee@codeclouders.com>
2021-11-21 16:15:28 +0200

    flutter course init

---
+++ lib/content.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Content extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child:const Icon(Icons.add),
+          ),
+          appBar: AppBar(
+            backgroundColor: const Color.fromRGBO(54, 44, 107, 1),
+            leading: const Icon(Icons.home),
+            title: const Text("Home Page"),
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        print('Button is Clicked!');
+                      },
+                      icon:const Icon(Icons.add_moderator),
+                      color: Colors.blue,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:app1/content.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Content());
+}
+
