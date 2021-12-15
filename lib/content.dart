d4a66f0b7b670298f97be798b6898b37a9ef1dd4
biscatcho <karimbiscatcho@gmail.com>
2021-12-15 14:49:08 +0200

    buttons and theme

--- lib/content.dart
+++ lib/content.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Content extends StatelessWidget {
+  pressBtn(name) {
+    print("Hello, $name");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "First App",
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        primaryColor: Colors.blue,
+        accentColor: Colors.red,
+        buttonTheme: const ButtonThemeData(
+            buttonColor: Colors.black12, textTheme: ButtonTextTheme.primary),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.brown),
+            elevation: MaterialStateProperty.all(10),
+          ),
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(
           leading: const Icon(Icons.home),
@@ -22,12 +40,59 @@ class Content extends StatelessWidget {
             )
           ],
         ),
-        body: Container(
-          child: const Text("Home Content"),
-          color: const Color(0xff4d6b55),
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(10),
-          margin: const EdgeInsets.symmetric(vertical: 10,horizontal: 15),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                FlatButton(onPressed: () {}, child: const Text("FlatBtn")),
+                OutlineButton(
+                    onPressed: () {}, child: const Text("OutLineBtn")),
+                RaisedButton(onPressed: () {}, child: const Text("RaisedBtn"))
+              ],
+            ),
+            Row(
+              children: [
+                TextButton(onPressed: () {}, child: const Text("TextBtn")),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: const Text("OutLinedBtn"),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("ElevatedBtn"),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                TextButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.one_k),
+                    label: const Text("TextBtn")),
+                OutlinedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.eleven_mp),
+                    label: const Text("OutLinedBtn")),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.ten_k),
+                  label: const Text("ElevatedBtn"),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () => pressBtn("karim osama"),
+                  icon: const Icon(
+                    Icons.one_k,
+                    // color: Colors.redAccent,
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
